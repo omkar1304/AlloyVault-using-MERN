651625c9d467a19431916974f6008c0d34689535
omkar1304 <omkarpedamkar2@gmail.com>
2025-04-17 15:48:58 +0530

    Update: CORS settings

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://alloyvault.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
