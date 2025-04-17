27f351b1dcbc949fe0c9a48d0f5be2d4a0ea814e
omkar1304 <omkarpedamkar2@gmail.com>
2025-04-17 15:57:00 +0530

    Update: CORS settings

--- backend/lib/generateToken.js
+++ backend/lib/generateToken.js
@@ -10,7 +10,7 @@ const generateToken = (userObj, res) => {
     httpOnly: true, // Prevent client-side JavaScript from accessing the cookie
     sameSite: 'none', // Required for cross-origin cookies
     secure: true, // Required for `sameSite: 'none'` (must be true in production)
-    domain: 'localhost', // Explicitly set the domain
+    // domain: 'localhost', // Explicitly set the domain
     path: '/', // Ensure the cookie is accessible across all paths
   });
 
